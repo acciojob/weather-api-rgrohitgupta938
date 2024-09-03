c4f40751d28efaf90eb37a35c78b649645891ce1
Rohit Gupta <49370602+rgrohitgupta938@users.noreply.github.com>
2024-09-03 19:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-//your JS code here. If required.
-const getBtn = document.getElementById("get-btn");
+const apiKey = "27cda249c0c147f31b3e9481324c2323";
+const weatherDataDiv = document.getElementById("weatherData");
+const getWeatherBtn = document.getElementById("getWeatherBtn");
 
-getBtn.addEventListener("click", () => {
-  const apiKey = "27cda249c0c147f31b3e9481324c2323";
-  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiKey}`;
+getWeatherBtn.addEventListener("click", () => {
+  const city = "London";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
   fetch(apiUrl)
-    .then((res) => res.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
     .then((data) => {
-      console.log(data.weather);
-      const weatherDiv = document.getElementById("weatherData");
-      weatherDiv.textContent = `Current weather in London:${data.weather[0].main}`;
+      const weatherDescription = data.weather[0].main;
+      weatherDataDiv.innerHTML = `Current weather in London: ${weatherDescription}`;
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+      weatherDataDiv.innerHTML = "Failed to load weather data.";
     });
 });
